e74b3c39aade7adfd37445db9925d73c22601119
Rhys Mainwaring <rhys.mainwaring@me.com>
2025-11-09 16:02:26 +0000

    Swarm: update ArduPilot Gazebo bridge
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

--- scripts/ardupilot_gazebo.py
+++ scripts/ardupilot_gazebo.py
@@ -2,6 +2,10 @@
 ArduPilot Gazebo Bridge - carry out similar functions to the ArduPilotPlugin
 
 Based on example: ardupilot/libraries/SITL/examples/JSON/pybullet/robot.py
+
+Example usage
+
+python ./src/ardupilot_gazebo/scripts/ardupilot_gazebo.py --world iris_runway --model iris_with_ardupilot_1
 """
 
 import copy
@@ -13,8 +17,10 @@ import struct
 import time
 
 from dataclasses import dataclass
+from dataclasses import field
 from argparse import ArgumentParser
 from pathlib import Path
+from threading import Lock
 from transforms3d import euler
 
 GZ_VERSION_GARDEN = "garden"
@@ -41,8 +47,13 @@ def gz_version():
 
 if gz_version() == GZ_VERSION_JETTY:
     from gz.msgs.boolean_pb2 import Boolean
+    from gz.msgs.clock_pb2 import Clock
     from gz.msgs.entity_factory_pb2 import EntityFactory
     from gz.msgs.entity_factory_v_pb2 import EntityFactory_V
+    from gz.msgs.imu_pb2 import IMU
+    from gz.msgs.model_pb2 import Model
+    from gz.msgs.pose_pb2 import Pose
+    from gz.msgs.pose_v_pb2 import Pose_V
     from gz.msgs.stringmsg_pb2 import StringMsg
 
 
@@ -66,7 +77,18 @@ class Control:
     offset: float = 0.0
     servo_min: float = 1000.0
     servo_max: float = 2000.0
-    output_ready: bool = False
+
+
+@dataclass
+class ArduPilotPlugin:
+    fdm_addr: str = ""
+    fdm_port: int = 9002
+    connection_timeout_max_count: int = 5
+    lock_step: bool = False
+    have_32_channels: bool = False
+    flu_to_frd: list[float] = field(default_factory=lambda: [0, 0, 0, 180, 0, 0])
+    enu_to_ned: list[float] = field(default_factory=lambda: [0, 0, 0, 180, 0, 90])
+    imu_name: str = ""
 
 
 class ArduPilotGazeboBridge:
@@ -103,21 +125,38 @@ class ArduPilotGazeboBridge:
 
         # Transport
         self.node = gz_node()
+        self.plugin = None
         self.controls = []
-        self.pub_commands = []
-        self.sub_clock = None
-        self.sub_imu = None
-        self.sub_pose_info = None
+        self.command_pubs = []
+
+        self.clock_lock = Lock()
+        self.clock_msg = Clock()
+        self.clock_sub = None
+        self.clock_topic = ""
+
+        self.imu_lock = Lock()
+        self.imu_msg = IMU()
+        self.imu_sub = None
+        self.imu_topic = ""
+
+        self.model_lock = Lock()
+        self.model_msg = IMU()
+        self.model_sub = None
+        self.model_topic = ""
+
+        self.pose_info_lock = Lock()
+        self.pose_info_msg = Pose_V()
+        self.pose_info_sub = None
+        self.pose_info_topic = ""
 
     def configure(self):
-        # Configure model
-        # TODO
-        # Hardcoded example to start
-        control = Control()
-        control.
+        # Load model
+        self._load_model()
 
+        # Setup Gazebo connections
+        self._init_pubsub()
 
-        # Setup connection
+        # Setup SITL connection
         self._init_sockets()
 
     def run(self):
@@ -126,6 +165,9 @@ class ArduPilotGazeboBridge:
         global TIME_STEP
 
         while True:
+            time.sleep(0.1)
+            continue
+
             # pre-update
             # TODO
             # self._receive_servo_packet()
@@ -207,6 +249,110 @@ class ArduPilotGazeboBridge:
 
                 self.frame_time = now
 
+    def _load_model(self):
+        # TODO replace hardcoded quadcopter example
+        self.plugin = ArduPilotPlugin()
+        self.plugin.fdm_addr = self.address
+        self.plugin.fdm_port = self.port
+        self.plugin.connection_timeout_max_count
+        self.plugin.lock_step = False
+        self.plugin.have_32_channels = False
+        self.plugin.flu_to_frd = [0, 0, 0, 180, 0, 0]
+        self.plugin.enu_to_ned = [0, 0, 0, 180, 0, 90]
+        self.plugin.imu_name = "iris_with_standoffs::imu_link::imu_sensor"
+
+        control = Control()
+        control.channel = 0
+        control.type = "COMMAND"
+        control.use_force = True
+        control.joint_name = "iris_with_standoffs::rotor_0_joint"
+        control.cmd_topic = "/model/iris_with_ardupilot/joint/rotor_0_joint/cmd_vel"
+        control.multipler = 838
+        control.offset = 0.0
+        control.servo_max = 2000
+        control.servo_min = 1000
+        self.controls.append(control)
+
+        control = Control()
+        control.channel = 1
+        control.type = "COMMAND"
+        control.use_force = True
+        control.joint_name = "iris_with_standoffs::rotor_1_joint"
+        control.cmd_topic = "/model/iris_with_ardupilot/joint/rotor_1_joint/cmd_vel"
+        control.multipler = 838
+        control.offset = 0.0
+        control.servo_max = 2000
+        control.servo_min = 1000
+        self.controls.append(control)
+
+        control = Control()
+        control.channel = 2
+        control.type = "COMMAND"
+        control.use_force = True
+        control.joint_name = "iris_with_standoffs::rotor_2_joint"
+        control.cmd_topic = "/model/iris_with_ardupilot/joint/rotor_2_joint/cmd_vel"
+        control.multipler = -838
+        control.offset = 0.0
+        control.servo_max = 2000
+        control.servo_min = 1000
+        self.controls.append(control)
+
+        control = Control()
+        control.channel = 3
+        control.type = "COMMAND"
+        control.use_force = True
+        control.joint_name = "iris_with_standoffs::rotor_3_joint"
+        control.cmd_topic = "/model/iris_with_ardupilot/joint/rotor_3_joint/cmd_vel"
+        control.multipler = -838
+        control.offset = 0.0
+        control.servo_max = 2000
+        control.servo_min = 1000
+        self.controls.append(control)
+
+    def _init_pubsub(self):
+        # gz.msgs.Clock
+        # /world/iris_runway/clock
+        self.clock_topic = (
+            f"/world/{self.world_name}/clock"
+        )
+        self.clock_sub = self.node.subscribe(Clock, self.clock_topic, self._clock_cb)
+        print(f"Subscribing to Clock on: {self.clock_topic}")
+
+        # gz.msgs.Pose_V
+        # /world/iris_runway/dynamic_pose/info
+        self.pose_info_topic = (
+            f"/world/{self.world_name}/dynamic_pose/info"
+        )
+        self.pose_info_sub = self.node.subscribe(Pose_V, self.pose_info_topic, self._pose_info_cb)
+        print(f"Subscribing to Pose_V on: {self.pose_info_topic}")
+
+        # gz.msgs.Model
+        # /world/iris_runway/model/iris_with_ardupilot_1/joint_state
+        self.model_topic = (
+            f"/world/{self.world_name}"
+            f"/model/{self.model_name}"
+            f"/joint_state"
+        )
+        self.model_sub = self.node.subscribe(Model, self.model_topic, self._model_cb)
+        print(f"Subscribing to Model on: {self.model_topic}")
+
+        # gz.msgs.IMU
+        # iris_with_standoffs::imu_link::imu_sensor
+        # /world/iris_runway/model/iris_with_ardupilot_1/model/iris_with_standoffs/link/imu_link/sensor/imu_sensor/imu
+        imu_split = self.plugin.imu_name.split("::")
+        imu_model = imu_split[0]
+        imu_link = imu_split[1]
+        imu_sensor = imu_split[2]
+        self.imu_topic = (
+            f"/world/{self.world_name}"
+            f"/model/{self.model_name}"
+            f"/model/{imu_model}"
+            f"/link/{imu_link}"
+            f"/sensor/{imu_sensor}/imu"
+        )
+        self.imu_sub = self.node.subscribe(IMU, self.imu_topic, self._imu_cb)
+        print(f"Subscribing to IMU on: {self.imu_topic}")
+
     def _init_sockets(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.bind((self.address, self.port))
@@ -239,6 +385,27 @@ class ArduPilotGazeboBridge:
     def _send_state(self):
         pass
         # self.sock.sendto(self.json_str, self.address, self.port)
+
+    def _clock_cb(self, msg):
+        with self.clock_lock:
+            self.clock_msg = copy.deepcopy(msg)
+            # print(self.clock_msg)
+
+    def _pose_info_cb(self, msg):
+        with self.pose_info_lock:
+            self.pose_info_msg = copy.deepcopy(msg)
+            # print(self.pose_info_msg)
+
+    def _model_cb(self, msg):
+        with self.model_lock:
+            self.model_msg = copy.deepcopy(msg)
+            # print(self.model_msg)
+
+
+    def _imu_cb(self, msg):
+        with self.imu_lock:
+            self.imu_msg = copy.deepcopy(msg)
+            # print(self.imu_msg)
 
 
 def main():
